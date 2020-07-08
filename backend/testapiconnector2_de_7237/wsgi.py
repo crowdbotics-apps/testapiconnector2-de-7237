05d41ca2f8154a91e5cb9b6d62902b0fce7aa0a1
Crowdbotics <team@crowdbotics.com>
2020-07-08 07:02:16 +0000

    Initial commit.

---
+++ backend/testapiconnector2_de_7237/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testapiconnector2_de_7237 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapiconnector2_de_7237.settings')
+
+application = get_wsgi_application()
